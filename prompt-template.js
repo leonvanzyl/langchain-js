495b6958bd0b32d321c2ced00114f9e449f712d7
Leon van Zyl <leon.vanzyl@gmail.com>
2024-01-09 14:18:37 +0200

    lesson-2

---
+++ prompt-template.js
@@ -0,0 +1,32 @@
+import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+
+// Import environment variables
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// Instantiate the model
+const model = new ChatOpenAI({
+  modelName: "gpt-3.5-turbo",
+  temperature: 0.9,
+});
+
+// Create Prompt Template using fromTemplate
+// const prompt = ChatPromptTemplate.fromTemplate('Tell a joke about {word}');
+
+// Create Prompt Template from fromMessages
+const prompt = ChatPromptTemplate.fromMessages([
+  [
+    "system",
+    "You are a talented chef.  Create a recipe based on a main ingredient provided by the user.",
+  ],
+  ["human", "{word}"],
+]);
+
+const chain = prompt.pipe(model);
+
+const response = await chain.invoke({
+  word: "dog",
+});
+
+console.log(response);
