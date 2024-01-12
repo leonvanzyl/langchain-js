447de95cf3db20dd93d7b8043b432c9b2310e976
Leon van Zyl <leon.vanzyl@gmail.com>
2024-01-12 08:36:45 +0200

    lesson-3

--- output-parser.js
+++ output-parser.js
@@ -3,8 +3,10 @@ import { ChatPromptTemplate } from "@langchain/core/prompts";
 import {
   CommaSeparatedListOutputParser,
   StringOutputParser,
-  BaseOutputParser,
 } from "@langchain/core/output_parsers";
+
+import { z } from "zod";
+import { StructuredOutputParser } from "langchain/output_parsers";
 
 // Import environment variables
 import * as dotenv from "dotenv";
@@ -16,32 +18,74 @@ const model = new ChatOpenAI({
   temperature: 0.9,
 });
 
-// Create Prompt Template from fromMessages
-const prompt = ChatPromptTemplate.fromMessages([
-  [
-    "system",
-    "Provide 5 synonyms, seperated by commas, for a word that the user will provide.",
-  ],
-  ["human", "{word}"],
-]);
+async function callStringOutputParser() {
+  const prompt = ChatPromptTemplate.fromTemplate("Tell a joke about {word}.");
+  const outputParser = new StringOutputParser();
 
-// Custom output parser
-class MyParser extends BaseOutputParser {
-  parse(output) {
-    console.log("Custom Parser:", output);
-    return output.split(",");
-  }
+  // Create the Chain
+  const chain = prompt.pipe(model).pipe(outputParser);
+
+  return await chain.invoke({ word: "dog" });
 }
 
-// Create the output parser
-// const outputParser = new CommaSeparatedListOutputParser();
-const outputParser = new MyParser();
+async function callListOutputParser() {
+  const prompt = ChatPromptTemplate.fromMessages([
+    [
+      "system",
+      "Provide 5 synonyms, seperated by commas, for a word that the user will provide.",
+    ],
+    ["human", "{word}"],
+  ]);
+  const outputParser = new CommaSeparatedListOutputParser();
 
-// Create the Chain
-const chain = prompt.pipe(model).pipe(outputParser);
+  const chain = prompt.pipe(model).pipe(outputParser);
 
-const response = await chain.invoke({
-  word: "chicken",
-});
+  return await chain.invoke({
+    word: "happy",
+  });
+}
 
+async function callStructuredParser() {
+  const prompt = ChatPromptTemplate.fromTemplate(
+    "Extract information from the following phrase.\n{format_instructions}\n{phrase}"
+  );
+
+  const outputParser = StructuredOutputParser.fromNamesAndDescriptions({
+    name: "name of the person",
+    age: "age of person",
+  });
+
+  const chain = prompt.pipe(model).pipe(outputParser);
+
+  return await chain.invoke({
+    phrase: "Max is 30 years old",
+    format_instructions: outputParser.getFormatInstructions(),
+  });
+}
+
+async function callZodStructuredParser() {
+  const prompt = ChatPromptTemplate.fromTemplate(
+    "Extract information from the following phrase.\n{format_instructions}\n{phrase}"
+  );
+  const outputParser = StructuredOutputParser.fromZodSchema(
+    z.object({
+      recipe: z.string().describe("name of recipe"),
+      ingredients: z.array(z.string()).describe("ingredients"),
+    })
+  );
+
+  // Create the Chain
+  const chain = prompt.pipe(model).pipe(outputParser);
+
+  return await chain.invoke({
+    phrase:
+      "The ingredients for a Spaghetti Bolognese recipe are tomatoes, minced beef, garlic, wine and herbs.",
+    format_instructions: outputParser.getFormatInstructions(),
+  });
+}
+
+// const response = await callStringOutputParser();
+// const response = await callListOutputParser();
+// const response = await callStructuredParser();
+const response = await callZodStructuredParser();
 console.log(response);
